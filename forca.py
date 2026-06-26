e58fcf3c7969f30030136102a8de8ba79e9c28a9
xcaua-cmyk <x.caua@escola.pr.gov.br>
2026-06-26 20:34:22 -0300

    Delete forca.py

--- forca.py
+++
@@ -1,9 +0,0 @@
-def jogar():
-    print("*********************************")
-    print("***Bem vindo ao jogo da Forca!***")
-    print("*********************************")
-
-    print("Fim do jogo")
-
-if(__name__ == "__main__"):
-    jogar()
